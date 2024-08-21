d59e326114ac2780f746cfdf4a9bcf87f95570d0
ManasaR05 <153731907+ManasaR05@users.noreply.github.com>
2024-08-21 17:00:43 +0100

    Sign up requester code fix (#13)
    Co-authored-by: Preetham Venkatesh <preetham.venkyy@gmail.com>
    Co-authored-by: PreethamVenkatesh <30716236+PreethamVenkatesh@users.noreply.github.com>

--- src/component/SignUpRequester.jsx
+++ src/component/SignUpRequester.jsx
@@ -1,18 +1,12 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import {
-  MDBBtn,
-  MDBContainer,
-  MDBCard,
   MDBCardBody,
-  MDBCardImage,
-  MDBRow,
-  MDBCol,
   MDBIcon,
   MDBInput
 } from 'mdb-react-ui-kit';
 import { useNavigate } from 'react-router-dom';
-import ambulanceImage from '../images/Ambulance4.jpg';
+import { Button } from 'react-bootstrap';
 
 function SignUpRequester() {
   const [formData, setFormData] = useState({
@@ -44,59 +38,55 @@ function SignUpRequester() {
   };
 
   return (
-    <div className="custom-bg">
-      <MDBContainer className="my-5">
-        <MDBCard>
-          <MDBRow className='g-1'>
-            <MDBCol md='7'>
-              <MDBCardImage src={ambulanceImage} alt="signup form" className='rounded-start w-100'/>
-            </MDBCol>
-            <MDBCol md='5'>
-              <MDBCardBody className='d-flex flex-column'>
-                <div className='d-flex flex-row mt-3'>
-                  <MDBIcon fas icon="cubes fa-3x me-3" style={{ color: '#ff6219' }}/>
-                  <span className="h1 fw-bold mb-0" style={{ fontSize: '3.5rem' }}>VAS LIFT ASSIST</span>
-                </div>
-
-                <div>
-                  <h3 className="text-center mb-4">Create your account</h3>
-                  <form onSubmit={handleSubmit}>
-                    <div className="input-wrapper mb-4">
-                      <label htmlFor="firstName" className="form-label">First Name</label>
-                      <MDBInput id='firstName' type='text' size="lg" className="flex-grow-1" value={formData.firstName} onChange={handleInputChange} required />
-                    </div>
-                    <div className="input-wrapper mb-4">
-                      <label htmlFor="lastName" className="form-label">Last Name</label>
-                      <MDBInput id='lastName' type='text' size="lg" className="flex-grow-1" value={formData.lastName} onChange={handleInputChange} required />
-                    </div>
-                    <div className="input-wrapper mb-4">
-                      <label htmlFor="emailId" className="form-label">Email address</label>
-                      <MDBInput id='emailId' type='email' size="lg" className="flex-grow-1" value={formData.emailId} onChange={handleInputChange} required />
-                    </div>
-                    <div className="input-wrapper mb-4">
-                      <label htmlFor="phoneNumber" className="form-label">Phone Number</label>
-                      <MDBInput id='phoneNumber' type='text' size="lg" className="flex-grow-1" value={formData.phoneNumber} onChange={handleInputChange} required />
-                    </div>
-                    <div className="input-wrapper mb-4">
-                      <label htmlFor="password" className="form-label">Password</label>
-                      <MDBInput id='password' type='password' size="lg" className="flex-grow-1" value={formData.password} onChange={handleInputChange} required />
-                    </div>
-                    <div className="input-wrapper mb-4">
-                      <label htmlFor="confirmPassword" className="form-label">Confirm Password</label>
-                      <MDBInput id='confirmPassword' type='password' size="lg" className="flex-grow-1" value={formData.confirmPassword} onChange={handleInputChange} required />
-                    </div>
-                    <MDBBtn className="w-100 mb-4" color='dark' size='lg' type='submit'>
-                      Register
-                    </MDBBtn>
-                  </form>
-                </div>
-              </MDBCardBody>
-            </MDBCol>
-          </MDBRow>
-        </MDBCard>
-      </MDBContainer>
+    <div>
+      <MDBCardBody style={{ marginLeft: "-5%", color: "whitesmoke" }}>
+        <div className='d-flex flex-row'>
+          <MDBIcon fas icon="cubes fa-3x me-3" style={{ color: '#ff6219' }} />
+          <span className="h1 fw-bold mb-0 mt-4" style={{ fontSize: '3.5rem', marginLeft: "40%" }}>VAS LIFT ASSIST</span>
+        </div>
+  
+        <div>
+          <form onSubmit={handleSubmit} className='text-center'>
+            <div style={{ marginRight: "50%" }}>
+              <h3>Create your account</h3>
+            </div>
+  
+            <div className='text-center mt-5'>
+              <div className="input-wrapper mb-4" style={{ marginLeft: "10%", width: "28%", display: "flex", justifyContent: "space-between" }}>
+                <label htmlFor="firstName" className="form-label">First Name</label>
+                <MDBInput id='firstName' type='text' size="lg" className="flex-grow-1" value={formData.firstName} onChange={handleInputChange} required />
+              </div>
+              <div className="input-wrapper mb-4" style={{ marginLeft: "10%", width: "28%", display: "flex", justifyContent: "space-between" }}>
+                <label htmlFor="lastName" className="form-label">Last Name</label>
+                <MDBInput id='lastName' type='text' size="lg" className="flex-grow-1" value={formData.lastName} onChange={handleInputChange} required />
+              </div>
+              <div className="input-wrapper mb-4" style={{ marginLeft: "10%", width: "28%", display: "flex", justifyContent: "space-between" }}>
+                <label htmlFor="emailId" className="form-label">Email address</label>
+                <MDBInput id='emailId' type='email' size="lg" className="flex-grow-1" value={formData.emailId} onChange={handleInputChange} required />
+              </div>
+              <div className="input-wrapper mb-4" style={{ marginLeft: "10%", width: "28%", display: "flex", justifyContent: "space-between" }}>
+                <label htmlFor="phoneNumber" className="form-label">Phone Number</label>
+                <MDBInput id='phoneNumber' type='text' size="lg" className="flex-grow-1" value={formData.phoneNumber} onChange={handleInputChange} required />
+              </div>
+              <div className="input-wrapper mb-4" style={{ marginLeft: "10%", width: "28%", display: "flex", justifyContent: "space-between" }}>
+                <label htmlFor="password" className="form-label">Password</label>
+                <MDBInput id='password' type='password' size="lg" className="flex-grow-1" value={formData.password} onChange={handleInputChange} required />
+              </div>
+              <div className="input-wrapper mb-4" style={{ marginLeft: "10%", width: "28%", display: "flex", justifyContent: "space-between" }}>
+                <label htmlFor="confirmPassword" className="form-label">Confirm Password</label>
+                <MDBInput id='confirmPassword' type='password' size="lg" className="flex-grow-1" value={formData.confirmPassword} onChange={handleInputChange} required />
+              </div>
+            </div>
+  
+            <Button type="submit" className='text-center mt-5' style={{ marginRight: "50%", fontSize: "25px" }}>
+              Register
+            </Button>
+          </form>
+        </div>
+      </MDBCardBody>
     </div>
   );
+  
 }
 
 export default SignUpRequester;
