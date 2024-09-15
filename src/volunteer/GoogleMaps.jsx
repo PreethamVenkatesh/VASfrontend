1853bc826a8988a1768edd4397ac237eed40813e
Preetham Venkatesh <preetham.venkyy@gmail.com>
2024-09-15 15:31:04 +0100

    Responsive UI updates

--- src/volunteer/GoogleMaps.jsx
+++ src/volunteer/GoogleMaps.jsx
@@ -1,10 +1,10 @@
-import { useJsApiLoader } from "@react-google-maps/api";
+import { GoogleMap, DirectionsRenderer, Marker, useJsApiLoader } from "@react-google-maps/api";
 
 const useGoogleMaps = () => {
   const { isLoaded, loadError } = useJsApiLoader({
     id: "google-map-script",
     googleMapsApiKey: "AIzaSyAyy8CB38wO_EDwAG8bO_WuKrO46JrvKt0", // Replace with your API key
-    // libraries: ['places', 'directions'], 
+    libraries: ['places', 'directions'], 
   });
 
   return { isLoaded, loadError };
